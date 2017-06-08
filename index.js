7f7691750ee0e43de1c24e2d6de32f1f61a5e36b
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-06-08 17:20:51 -0600

    add GCF interface and fix git pipeline. PRs now work for helm->helm

---
+++ index.js
@@ -0,0 +1,117 @@
+// (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+const config = require('./lib/config');
+const functions = require('./lib/functions');
+
+const { ExtendableError } = require('./lib/util');
+
+process.on('unhandledRejection', (reason) => {
+  console.log('unhandled rejection!', reason.stack);
+});
+
+class HttpError extends ExtendableError {
+  constructor(m, code) {
+    super(m);
+
+    this.code = code;
+  }
+}
+
+function verifyToken(req) {
+  const cfg = config.get();
+  if (!cfg.tokens.find(t => t === req.body.token)) {
+    throw new HttpError('unauthorized', 401);
+  }
+
+  return Promise.resolve(req);
+}
+
+function doGet(req) {
+  const action = req.body.action;
+  if (!action) {
+    throw new HttpError('`action` must be provided', 400);
+  }
+  
+  switch (action) {
+    case 'getRepository':
+      return functions.getRepository(req.body.name);
+    case 'listRepositories':
+      return functions.listRepositories();
+    case 'getRepositoryByRemote':
+      return functions.getRepositoryByRemote(req.body.remote);
+    case 'listDependents':
+      return functions.listDependents(
+          req.body.repoName,
+          req.body.moduleName,
+          req.body.moduleType);
+    default:
+      throw new HttpError(`invalid action: ${action}`, 400);
+  }
+}
+
+function doPost(req) {
+  const action = req.body.action;
+  if (!action) {
+    throw new HttpError('`action` must be provided', 400);
+  }
+
+  switch (action) {
+    case 'addRepository': 
+      return functions.addRepository(
+          req.body.name,
+          req.body.type,
+          req.body.remote,
+          req.body.parent || null);
+    case 'removeRepository':
+      return functions.removeRepository(req.body.name);
+    case 'softUpdateRepository':
+      return functions.softUpdateRepository(req.body.name);
+    default:
+      throw new HttpError(`invalid action: ${action}`, 400);
+  }
+}
+
+function bot(req, res) {
+  if (req.get('content-type') !== 'application/json') {
+    res.send(406).send('content-type must be application/json');
+    return;
+  }
+
+  let func;
+  if (req.method === 'GET') {
+    func = doGet;
+  } else if (req.method === 'POST') {
+    func = doPost;
+  } else {
+    func = () => {
+      return Promise.reject(new HttpError(
+        `method not allowed: ${req.method}`, 405));
+    };
+  }
+
+  verifyToken(req).then(func).then(response => {
+    res.status(200).send(response).end();
+  }).catch(err => {
+    if (err instanceof HttpError) {
+      res.status(err.code).send(err.message).end();
+    } else {
+      console.log('unhandled error:', err);
+      res.status(500).send(err.message).end();
+    }
+  });
+}
+
+module.exports = { bot };
+
